ff6f39de237c284b0b9007f7a5a954bb93a14681
EnigmaticZee <zaidshakilz@gmail.com>
2018-03-20 12:27:58 +1100

    Update

--- 4.Find the Longest Word in a String.js
+++ 4.Find the Longest Word in a String.js
@@ -1,11 +1,11 @@
-
 function findLongestWord(str) {
-  var words  =str.split(" ");
-  var longest = "";
-  for (var word of words){
-    if (word.length > longest.length){longest = word;}
-  }
-  return longest.length;
+	return str.split(" ").sort(function(a,b){return b.length - a.length})[0].length;
+  // var words  =str.split(" ");
+  // var longest = "";
+  // for (var word of words){
+  //   if (word.length > longest.length){longest = word;}
+  // }
+  // return longest.length;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
